50c3090dbbd02125b9c198afe21ad554ecf8a857
T3NED <software@t3ned.com>
2022-04-23 22:49:54 +0100

    Add prettier configuration file

---
+++ packages/prettier-config/src/index.ts
@@ -0,0 +1,15 @@
+export const prettierConfig = {
+	semi: true,
+	singleQuote: false,
+	trailingComma: "all",
+	arrowParens: "always",
+	tabWidth: 4,
+	useTabs: true,
+	bracketSpacing: true,
+	proseWrap: "always",
+	printWidth: 110,
+	endOfLine: "auto",
+};
+
+module.exports = prettierConfig;
+export default prettierConfig;
