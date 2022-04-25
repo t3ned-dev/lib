99de676365ec25bac2b20c0111d38e435fd22ebe
T3NED <software@t3ned.com>
2022-04-25 21:38:50 +0100

    Add missing exports of logger

--- packages/logger/src/lib/index.ts
+++ packages/logger/src/lib/index.ts
@@ -1,2 +1,6 @@
+export * from "./struct/transports/Console";
+export * from "./struct/transports/File";
+export * from "./struct/transports/Sentry";
+export * from "./struct/Logger";
 export * from "./util/timestampFormat/timestampFormat";
 export * from "./util/consoleFormat/consoleFormat";
